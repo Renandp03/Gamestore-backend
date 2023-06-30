fe2286bcd3261219b8bfad6d5edb500732c4edb6
ElderGr <gbielrs@gmail.com>
2023-06-30 17:46:00 -0300

    feat: code review

--- src/errors/user-email-already-exist-error.ts
+++
@@ -1,6 +0,0 @@
-export default function userEmailAlreadyExistError(){
-    return {
-        name:'Email already exist error.',
-        message:'This email can not be used.',
-    }
-}
--- src/schemas/postGame-schema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-const postGameSchema = Joi.object({
-    name: Joi.string().min(2).max(50).required(),
-    image:Joi.string().required(),
-    consoleName:Joi.string().required()
-})
-
-export default postGameSchema;
